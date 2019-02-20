91befe88e3518f20176dfc863da13997ea31fc5e
Marcel Eschmann <marcel.eschmann@gmail.com>
2019-02-20 22:03:29 +0100

    Fix namespace

--- Twig/MenuExtension.php
+++ Twig/MenuExtension.php
@@ -4,7 +4,7 @@ namespace Eschmar\MenuBundle\Twig;
 
 use Eschmar\MenuBundle\Helper\MenuNameParser;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Bundle\FrameworkBundle\Routing\RouterInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Twig Extension providing menu rendering functions
